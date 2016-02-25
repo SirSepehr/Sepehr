fd51192d112e88bfdf29a2c18232287d2fcbf4ac
Sepehr <sepehr.99pvg@gmail.com>
2016-02-25 00:34:29 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {B_D_Hacker},
+    sudo_users = {@B_D_Hacker},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
